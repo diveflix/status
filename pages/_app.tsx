939925fd07949f96c170338db109a7e7698510b0
Diveflix <144784941+diveflix@users.noreply.github.com>
2023-09-12 14:28:09 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<title>System Status</title>
+				<title>Diveflix Status</title>
 			</Head>
 			<Component {...pageProps} />
 		</>
